6e87c8d4da9cf6c263840582f386857c3051221a
Umesh Dhangare <umesh.dhangare@nobrainsolutions.com>
2021-09-25 09:32:22 +0530

    Profile

--- src/app/reactiveforms/profile-editor/profile-editor.component.ts
+++ src/app/reactiveforms/profile-editor/profile-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from 'src/app/forbidden-name-directive';
 
 @Component({
   selector: 'app-profile-editor',
@@ -19,7 +20,7 @@ export class ProfileEditorComponent implements OnInit {
   // });
 
   profileForm = this.fb.group({
-    firstName: [''],
+    firstName: ['',[Validators.minLength(1), forbiddenNameValidator(/bob/i)]],
     lastName:[''],
     address:this.fb.group({
       street:[''],
@@ -28,7 +29,7 @@ export class ProfileEditorComponent implements OnInit {
       zip:['']
     }),
     aliases:this.fb.array([
-      this.fb.control('')
+      this.fb.control('Alias')
     ])
   })
 
@@ -51,8 +52,12 @@ export class ProfileEditorComponent implements OnInit {
   updateProfile(){
     this.profileForm.patchValue({
       firstName:'Jack',
+      lastName:'Jones',
       address:{
-        street:'123 street'
+        street:'123 street',
+        city:'Bombay',
+        state:'Maharashtra',
+        zip:'400076'
       }
     });
   }
